89b4822f8405e8822e6aaf0cb0927f70fd9c7c90
csut017 <cj.sutherland@auckland.ac.nz>
2025-11-07 11:45:00 +1300

    Added a console application to perform quick tests of any functionality.

---
+++ QuickTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using Canvas.Core;
+using Canvas.Core.Http;
+using Canvas.Core.Settings;
+using QuickTest;
+
+var settings = AppSettings.New<Program>("appsettings.json");
+
+Console.WriteLine($"Canvas URL is {settings.CanvasUrl}");
+var client = new ClientConfiguration()
+    .ViaHttp(settings.CanvasUrl, settings.CanvasToken)
+    .Build();
+var user = await client.CurrentUser.Retrieve();
+Console.WriteLine($"Current user is {user?.Name}");
+
+var count = 0;
+await foreach (var course in client.Courses.ListForCurrentUser(new CourseList { PageSize = List.MaxPageSize }))
+{
+    Console.WriteLine($"Course #{++count}: {course.Name} [{course.Term?.Name}]");
+}
